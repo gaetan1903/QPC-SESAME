c843b93e0dc208d280641aa6ba7c5c2f5a36df2f
Gaetan Jonathan BAKARY <gaetan.s118@gmail.com>
2019-04-27 19:41:34 +0300

    Skip pas encore fonctionnel

--- Interface.py
+++ Interface.py
@@ -1,41 +1,60 @@
-from tkinter import * 
+#-*-coding: utf-8 -*-
+# __author__ Gaetan Jonathan
+
+from tkinter import *
+import pygame 
 import threading, time
+
+pygame.init()
+
+class Intro(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)  # je precise que cette classe est une classe qui est lancé en tant que thread
+ 
+
+    def run(self):  # la fonction qui va etre lancer en threading
+        self.etat = True
+        import anim
+        self.etat = False
 
 
 
 class Interface():
     def __init__(self):
-        self.root = Tk()
+        self.root = Tk()  #  creation de ma fenetre
         self.root.title('QPC SESAME')
-        self.root.geometry('1200x600')
+        self.root.geometry('1200x600')  # taille de la fenetre 
+        self.root.resizable(width=False, height=False)  #  empecher le redimensionnement
 
     
     def __final__(self):
-        self.root.mainloop()
+        self.root.mainloop()  #  lancement de la fenetre
 
 
-class Intro(threading.Thread):
+class Sound(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-
+        self.son = pygame.mixer.Sound("animation.wav")
 
     def run(self):
-        self.etat = True
-        import anim
-        self.etat = False  
+        self.son.play()
 
-
+        
 
 if __name__ =='__main__':
-    thread1 = Intro()
-    thread1.start()
-    time.sleep(2)
-
-    while thread1.etat: # J'attend que le thread s'arrete
-        time.sleep(0.1)    
     
-    print('hello guys')
+    thread1 = Intro()  #  instanciation du classe qui contient le thread
+    thread2 = Sound()
 
-    fen = Interface()
+    thread1.start()
+    thread2.start()
+
+    time.sleep(2)  #  mettre en pause pendant 2s pour assurer que la classe thread demarre
+
+    while thread1.etat : # J'attend que le thread s'arrete
+        time.sleep(0.1)
+
+       
+    fen = Interface()   #  lanceons now notre fenetre
     fen.__final__()
     
--- anim.py
+++ anim.py
@@ -1,3 +1,6 @@
+#-*-coding: utf-8 -*-
+# __author__ Gaetan Jonathan
+
 from PyQt5.QtCore import (Qt, QSize,QByteArray, pyqtSlot, pyqtSignal, QThread)
 from PyQt5.QtGui import (QMovie)
 from PyQt5.QtWidgets import (QApplication, QWidget,QSizePolicy,QVBoxLayout, QPushButton, QLabel, QGridLayout)
@@ -5,26 +8,28 @@ from PyQt5.QtWidgets import (QApplication, QWidget,QSizePolicy,QVBoxLayout, QPus
 import threading
 import time
 import sys
+import Interface
 
-
+verification = False
 
 class ImagePlayer(QWidget):
     def __init__(self, filename, parent=None):
         QWidget.__init__(self, parent)
 
-        # Load the file into a QMovie
+        # charger le fichier dans QMovie
         self.movie = QMovie(filename, QByteArray(), self)
 
         size = self.movie.scaledSize()
         self.setGeometry(600, 340, size.width(), size.height())
+        self.move(350, 125)
         self.setWindowTitle("QPC SESAME")
 
         self.movie_screen = QLabel()
-        # Make label fit the gif
+        # mettre le label du gif 
         self.movie_screen.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.movie_screen.setAlignment(Qt.AlignCenter)
 
-        # Create the layout
+        # Creation des layout 
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.movie_screen)
 
@@ -34,12 +39,13 @@ class ImagePlayer(QWidget):
         self.movie.setCacheMode(QMovie.CacheAll)
         self.movie.setSpeed(100)
         self.movie_screen.setMovie(self.movie)
-        self.movie.start()
+        self.movie.start()  #  lancer le gif 
 
 
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            thread_1.app.quit()
+    def mousePressEvent(self, event):  #  fonction d evenement de touche de souris 
+        if event.button() == Qt.LeftButton:  #  si  l evenement du click est click gauche
+            
+            thread_1.app.quit()   #  arrete l application 
         
 
     
@@ -58,14 +64,11 @@ class Launch(threading.Thread):
 
 
         
-
-
-
 thread_1 = Launch()
 
 thread_1.start()
 
-time.sleep(5)
+time.sleep(20)
 thread_1.app.quit()
 
  
