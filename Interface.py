d1df8323859be404ba060b1e70aa3a81ce8023e3
Gaetan Jonathan BAKARY <gaetan.s118@gmail.com>
2019-04-28 02:22:08 +0300

    Premier page fonctionnel

--- Interface.py
+++ Interface.py
@@ -3,6 +3,7 @@
 
 from tkinter import * 
 import threading, time
+import tkinter.font as tkFont
 
 
 class Intro(threading.Thread):
@@ -20,12 +21,19 @@ class Intro(threading.Thread):
 class Interface():
     def __init__(self):
         self.root = Tk()  #  creation de ma fenetre
-        self.root.title('QPC SESAME')
+        self.root.title('Question Pour un Champion SESAME')
         self.root.geometry('1200x600+100+50')  # taille de la fenetre 
         self.root.resizable(width=False, height=False)  #  empecher le redimensionnement
+
+        self.helv32 = tkFont.Font(family='Helvetica', size=32, weight='bold')
+
+        self.offline0 = PhotoImage(file='offline.png')
+        self.poussoir0 = PhotoImage(file='poussoir.png')
         self.fsociety0 = PhotoImage(file='logo.png')
         self.esti0 = PhotoImage(file='esti.png')
         self.sesame0 = PhotoImage(file='sesame.png')
+        self.reseau0 = PhotoImage(file='reseau.png')
+         
 
     def __corps__(self):
         self.eval = Canvas(self.root, bg = 'white', width = 1200, height = 75)
@@ -34,6 +42,9 @@ class Interface():
         self.fsociety = self.fsociety0.subsample(4,4)
         self.esti = self.esti0.subsample(10, 10)
         self.sesame = self.sesame0.subsample(20, 20)
+        self.offline = self.offline0.subsample(6, 6)
+        self.poussoir = self.poussoir0.subsample(2, 2)
+        self.reseau = self.reseau0.subsample(2,2)
 
         self.eval.create_image(600, 43 , image = self.fsociety)
         self.eval.create_image(60, 37 , image = self.esti)
@@ -45,6 +56,19 @@ class Interface():
         self.footer.create_text(1120, 25, text = '© Copyright Juin 2019', activefill = 'yellow')
         self.footer.create_text(115, 25, text = '♣ Licence Libre && Open Source', activefill = 'yellow')
         self.footer.create_text(625, 25, text = '☻  ambatoroka.f-society@gmail.com', activefill = 'yellow')
+
+        self.offlineButton = Button(self.root, bd =0,  image = self.offline)
+        self.offlineButton.place(relx = 0.03, rely = 0.25)
+        self.offlineLabel = Label(self.root, text = 'HORS LIGNE', activeforeground = 'yellow', font = self.helv32).place(relx = 0.035, rely = 0.7)
+
+        self.poussoirButton = Button(self.root, bd = 0, image = self.poussoir)
+        self.poussoirButton.place(relx = 0.4, rely = 0.25)
+        self.poussoirLabel = Label(self.root, text = 'POUSSOIR', activeforeground = 'yellow', font = self.helv32).place(relx = 0.4, rely = 0.7)
+
+        self.reseauButton = Button(self.root, bd = 0, image = self.reseau)
+        self.reseauButton.place(relx = 0.75, rely = 0.25)
+        self.reseauLabel = Label(self.root, text = 'EN RESEAU', activeforeground = 'yellow', font = self.helv32).place(relx = 0.755, rely = 0.7)
+
 
     
     def __final__(self):
