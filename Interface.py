28dd8725a5db5802eb3e62c0a57969e7d7666d8d
Gaetan Jonathan BAKARY <gaetan.s118@gmail.com>
2019-04-30 21:15:58 +0300

    solution apporter au evenement de survolage de souris

--- Interface.py
+++ Interface.py
@@ -5,6 +5,7 @@ from tkinter import *
 import threading, time
 import tkinter.font as tkFont
 import tkinter.messagebox as tkmsg
+from tkinter.filedialog import *
 
 
 class Intro(threading.Thread):
@@ -22,13 +23,14 @@ class Intro(threading.Thread):
 class Interface():
     def __init__(self):
         self.root = Tk()  #  creation de ma fenetre
-        self.root.title('Question Pour un Champion SESAME')
+        self.root.title('Menu Principale')
         self.root.geometry('1200x600+100+50')  # taille de la fenetre
-        self.root['bg'] = '#032f62'
+        self.root['bg'] = 'white'
         self.root.resizable(width=False, height=False)  #  empecher le redimensionnement
 
         self.helv32 = tkFont.Font(family='Helvetica', size=32, weight='bold')
         self.helv36 = tkFont.Font(family='Arial', size=36, weight='bold')
+        self.arial28 = tkFont.Font(family='Arial', size=28, weight='bold')
 
         self.offline0 = PhotoImage(file='offline.png')
         self.poussoir0 = PhotoImage(file='poussoir.png')
@@ -45,28 +47,47 @@ class Interface():
         self.eval = Canvas(self.root, bg = 'white', width = 1200, height = 75)
         self.eval.place(relx = -0.001, rely = -0.001)
        
-        self.fsociety = self.fsociety0.subsample(4,4)
+        self.fsociety = self.fsociety0.subsample(8, 8)
         self.esti = self.esti0.subsample(10, 10)
         self.sesame = self.sesame0.subsample(20, 20)
         self.offline = self.offline0.subsample(6, 6)
         self.poussoir = self.poussoir0.subsample(2, 2)
         self.reseau = self.reseau0.subsample(2,2)
 
-        self.eval.create_image(600, 43 , image = self.fsociety)
-        self.eval.create_image(60, 37 , image = self.esti)
+        #self.eval.create_image(600, 43 , image = self.fsociety)
+        self.eval.create_image(90, 37 , image = self.fsociety)
         self.eval.create_image(1140, 37 , image = self.sesame)
 
-        self.footer = Canvas(self.root, bg = 'teal', width = 1200, height = 50, bd = 0)
+        self.footer = Canvas(self.root, bg = 'teal', width = 1202, height = 50, bd = 0, highlightthickness = 0)
         self.footer.place(relx = -0.001, rely = 0.92)
 
         self.footer.create_text(1120, 25, text = '© Copyright Juin 2019', activefill = 'orange', fill ='yellow')
         self.footer.create_text(115, 25, text = '♣ Licence Libre && Open Source', activefill = 'orange', fill ='yellow')
-        self.footer.create_text(620, 25, text = '☻  ambatoroka.f-society@gmail.com', activefill = 'orange', fill ='yellow')
+        self.footer.create_text(620, 25, text = '☻  ambatoroka.fsociety@gmail.com', activefill = 'orange', fill ='yellow')
 
+        self.menuJeu = Canvas(self.root, bg = 'teal', highlightthickness = 0, width = 1202, height = 65)
+        self.menuJeu.place(relx = - 0.001, rely = 0.1278)
+
+        self.offlineButton = Button(self.root, bd = 0, fg = 'white', relief = 'groove',  bg = 'teal', activeforeground = 'orange', activebackground = 'teal', text = "Mode Offline",  font = self.arial28, command = self.offlineCommand)
+        self.offlineButton.place(relx = 0.005, rely = 0.137)
+
+        self.offlineButton.bind("<Enter>", self.offlinemouseOverEnter)
+        self.offlineButton.bind("<Leave>", self.offlinemouseOverLeave)
+
+        self.poussoirButton = Button(self.root, bd = 0, fg = 'white', relief = 'groove',  bg = 'teal', activeforeground = 'orange', activebackground = 'teal', text = "Mode Poussoir",  font = self.arial28, command = self.offlineCommand)
+        self.poussoirButton.place(relx = 0.37, rely = 0.137)
+
+        self.reseauButton = Button(self.root, bd = 0, fg = 'white', relief = 'groove',  bg = 'teal', activeforeground = 'orange', activebackground = 'teal', text = "Mode Reseau",  font = self.arial28, command = self.offlineCommand)
+        self.reseauButton.place(relx = 0.77, rely = 0.137)
+        #self.menuJeu.create_text(150, 30, text = "Mode Offline", activefill = 'orange', fill = "white", font = self.arial28, tags ='offline')
+        #self.menuJeu.create_text(600, 30, text = "Mode Poussoir", activefill = 'orange', fill = "white", font = self.arial28, tags = 'poussoir')
+        #self.menuJeu.create_text(1060, 30, text = "Mode Reseau", activefill = 'orange', fill = "white", font = self.arial28, tags ='reseau')
+
+        """
         self.ModeJeuLabel = Label(self.root, text = 'MODE DE JEU', bg ='#032f62', fg = 'white', font = self.helv36).place(relx = 0.36, rely = 0.13)
 
-        self.offlineButton = Button(self.root, bd =0, bg ='#032f62',  image = self.offline, command = self.offlineCommand)
-        self.offlineButton.place(relx = 0.03, rely = 0.3)
+       
+        self.offlineButton.place(relx = -0.001, rely = 0.3)
         self.offlineLabel = Label(self.root, text = 'HORS LIGNE', bg = '#032f62', fg ='white',  font = self.helv32).place(relx = 0.035, rely = 0.75)
 
         self.poussoirButton = Button(self.root, bd = 0, bg ='#032f62', image = self.poussoir, command = self.poussoirCommand)
@@ -76,11 +97,21 @@ class Interface():
         self.reseauButton = Button(self.root, bd = 0, bg ='#032f62', image = self.reseau, command = self.reseauCommand)
         self.reseauButton.place(relx = 0.75, rely = 0.3)
         self.reseauLabel = Label(self.root, text = 'EN RESEAU', bg = '#032f62',fg ='white', font = self.helv32).place(relx = 0.755, rely = 0.75)
-
+        """
     def on_Close(self):
             self.count = 0  #  retour au zero du compteur
             self.fen_f1.destroy()
 
+
+    def openProject(self):
+        self.file = askopenfilename(filetypes=[(".gj","*.gj")])
+
+    
+    def offlinemouseOverEnter(self, event):
+        print('solution trouver')
+
+    def offlinemouseOverLeave(self, event):
+        print('bye2 Ntsoa')
 
     def offlineCommand(self):
         self.count += 1 
@@ -94,7 +125,7 @@ class Interface():
 
             newButton = Button(self.fen_f1, text = 'Nouveau Projet', activeforeground ='teal')
             newButton.place(relx = 0.05, rely = 0.45)
-            openButton = Button(self.fen_f1, text = 'Ouvrir un Projet', activeforeground ='#032f62').place(relx = 0.65, rely = 0.45)
+            openButton = Button(self.fen_f1, text = 'Ouvrir un Projet', activeforeground ='#032f62', command = self.openProject).place(relx = 0.65, rely = 0.45)
             self.fen_f1.protocol("WM_DELETE_WINDOW", self.on_Close)  #  evenement en cas de fermeture 
             self.fen_f1.mainloop()
 
