18a38fe42171e17e3f9cb3798d768d7a94872dc6
Gaetan Jonathan BAKARY <gaetan.s118@gmail.com>
2019-04-30 12:17:01 +0300

    amelioration des couleurs en cours

--- Interface.py
+++ Interface.py
@@ -23,11 +23,12 @@ class Interface():
     def __init__(self):
         self.root = Tk()  #  creation de ma fenetre
         self.root.title('Question Pour un Champion SESAME')
-        self.root.geometry('1200x600+100+50')  # taille de la fenetre 
+        self.root.geometry('1200x600+100+50')  # taille de la fenetre
+        self.root['bg'] = '#032f62'
         self.root.resizable(width=False, height=False)  #  empecher le redimensionnement
 
         self.helv32 = tkFont.Font(family='Helvetica', size=32, weight='bold')
-        self.helv36 = tkFont.Font(family='Helvetica', size=36, underline = 1, weight='bold', slant = 'italic')
+        self.helv36 = tkFont.Font(family='Arial', size=36, weight='bold')
 
         self.offline0 = PhotoImage(file='offline.png')
         self.poussoir0 = PhotoImage(file='poussoir.png')
@@ -55,41 +56,54 @@ class Interface():
         self.eval.create_image(60, 37 , image = self.esti)
         self.eval.create_image(1140, 37 , image = self.sesame)
 
-        self.footer = Canvas(self.root, bg = 'teal', width = 1200, height = 50)
+        self.footer = Canvas(self.root, bg = 'teal', width = 1200, height = 50, bd = 0)
         self.footer.place(relx = -0.001, rely = 0.92)
 
-        self.footer.create_text(1120, 25, text = '© Copyright Juin 2019', activefill = 'yellow')
-        self.footer.create_text(115, 25, text = '♣ Licence Libre && Open Source', activefill = 'yellow')
-        self.footer.create_text(620, 25, text = '☻  ambatoroka.f-society@gmail.com', activefill = 'yellow')
+        self.footer.create_text(1120, 25, text = '© Copyright Juin 2019', activefill = 'orange', fill ='yellow')
+        self.footer.create_text(115, 25, text = '♣ Licence Libre && Open Source', activefill = 'orange', fill ='yellow')
+        self.footer.create_text(620, 25, text = '☻  ambatoroka.f-society@gmail.com', activefill = 'orange', fill ='yellow')
 
-        self.ModeJeuLabel = Label(self.root, text = 'MODE DE JEU', fg ='#032f62', activeforeground = 'yellow', font = self.helv36).place(relx = 0.36, rely = 0.13)
+        self.ModeJeuLabel = Label(self.root, text = 'MODE DE JEU', bg ='#032f62', fg = 'white', font = self.helv36).place(relx = 0.36, rely = 0.13)
 
-        self.offlineButton = Button(self.root, bd =0,  image = self.offline, command = self.offlineCommand)
+        self.offlineButton = Button(self.root, bd =0, bg ='#032f62',  image = self.offline, command = self.offlineCommand)
         self.offlineButton.place(relx = 0.03, rely = 0.3)
-        self.offlineLabel = Label(self.root, text = 'HORS LIGNE', fg = '#032f62', font = self.helv32).place(relx = 0.035, rely = 0.75)
+        self.offlineLabel = Label(self.root, text = 'HORS LIGNE', bg = '#032f62', fg ='white',  font = self.helv32).place(relx = 0.035, rely = 0.75)
 
-        self.poussoirButton = Button(self.root, bd = 0, image = self.poussoir, command = self.poussoirCommand)
+        self.poussoirButton = Button(self.root, bd = 0, bg ='#032f62', image = self.poussoir, command = self.poussoirCommand)
         self.poussoirButton.place(relx = 0.41, rely = 0.3)
-        self.poussoirLabel = Label(self.root, text = 'POUSSOIR', fg = '#032f62', font = self.helv32).place(relx = 0.41, rely = 0.75)
+        self.poussoirLabel = Label(self.root, text = 'POUSSOIR', bg = '#032f62', fg ='white',font = self.helv32).place(relx = 0.41, rely = 0.75)
 
-        self.reseauButton = Button(self.root, bd = 0, image = self.reseau, command = self.reseauCommand)
+        self.reseauButton = Button(self.root, bd = 0, bg ='#032f62', image = self.reseau, command = self.reseauCommand)
         self.reseauButton.place(relx = 0.75, rely = 0.3)
-        self.reseauLabel = Label(self.root, text = 'EN RESEAU', fg = '#032f62', font = self.helv32).place(relx = 0.755, rely = 0.75)
+        self.reseauLabel = Label(self.root, text = 'EN RESEAU', bg = '#032f62',fg ='white', font = self.helv32).place(relx = 0.755, rely = 0.75)
+
+    def on_Close(self):
+            self.count = 0  #  retour au zero du compteur
+            self.fen_f1.destroy()
 
 
     def offlineCommand(self):
         self.count += 1 
-        if self.count <= 1:
-            fen_f1 = Toplevel(self.root)
-            fen_f1.title('QPC SESAME')
-            fen_f1.geometry('400x100')
-            self.projet = self.projet0.subsample(6, 6)
-            projetImage = Label(fen_f1, image = self.projet).place(relx = 0.39, rely = 0.10)
 
-            newButton = Button(fen_f1, text = 'Nouveau Projet', activeforeground ='teal')
+        if self.count <= 1:
+            self.fen_f1 = Toplevel(self.root)
+            self.fen_f1.title('QPC SESAME')
+            self.fen_f1.geometry('400x100')
+            self.projet = self.projet0.subsample(6, 6)
+            projetImage = Label(self.fen_f1, image = self.projet).place(relx = 0.39, rely = 0.10)
+
+            newButton = Button(self.fen_f1, text = 'Nouveau Projet', activeforeground ='teal')
             newButton.place(relx = 0.05, rely = 0.45)
-            openButton = Button(fen_f1, text = 'Ouvrir un Projet', activeforeground ='#032f62').place(relx = 0.65, rely = 0.45)
-            fen_f1.mainloop()
+            openButton = Button(self.fen_f1, text = 'Ouvrir un Projet', activeforeground ='#032f62').place(relx = 0.65, rely = 0.45)
+            self.fen_f1.protocol("WM_DELETE_WINDOW", self.on_Close)  #  evenement en cas de fermeture 
+            self.fen_f1.mainloop()
+
+        else:
+            self.fen_f1.withdraw()  #  cache la fenetre 
+            self.fen_f1.deiconify()  #  reaffiche la fenetre
+
+        
+        
 
         """
         fen.root.withdraw()
